c687078c6d3c3df5d95d515ca0523e4bb48e151c
Rohit Pandit <rohitpandit135@outlook.com>
2025-03-31 00:03:31 +0530

    Initial Setup

---
+++ LearnDSA/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LearnDSA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RecursionCheck recursionCheck = new RecursionCheck();
+
+            recursionCheck.FindSquareIteratively(4);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LearnDSA/RecursionCheck.cs
@@ -0,0 +1,26 @@
+﻿namespace LearnDSA
+{
+    public class RecursionCheck
+    {
+        public void FindSquareIteratively(int n)
+        {
+            List<int> result = new();
+            while(n > 0)
+            {
+                Console.WriteLine(n * n);
+                n--;
+            }
+        }
+
+        public void FindSquareRecursively(int n)
+        {
+            List<int> result = new();
+            if (n > 0)
+            {
+                Console.WriteLine(n * n);
+                FindSquareRecursively(n - 1);
+            }
+        }
+
+    }
+}
